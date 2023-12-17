66b6e42c71982c79035f404e7c8269384bdf1208
KyleRobison15 <kylerobison15@gmail.com>
2023-12-17 16:15:51 -0700

    initial commit

---
+++ src/test/java/com/krd/letsclimbrest/LetsClimbRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krd.letsclimbrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsClimbRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
